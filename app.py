a8dcd9404ecc69b5ad064ff3d34196734bcf42cc
ujjawal pandey <ujjawalpandeyup125@gmail.com>
2025-12-31 13:43:36 +0530

    Update website name in home route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to My Hemant  Website</h1><p>This is my first site.</p>"
+    return "<h1>Welcome to My Ujjawal Website</h1><p>This is my first site.</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
